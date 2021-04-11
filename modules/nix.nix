33f98355be66d77f002f856707882abd1251e65c
Martin Toman <toman.martin@live.com>
2021-04-11 20:47:50 +0200

    Add hydra.iohk.io binary cache (haskell.nix)

--- modules/nix.nix
+++ modules/nix.nix
@@ -11,6 +11,14 @@
     max-free = ${toString (1024 * 1024 * 1024)}
   '';
 
+  # Binary caches
+  nix.binaryCachePublicKeys = [
+  "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+  ];
+  nix.binaryCaches = [
+    "https://hydra.iohk.io"
+  ];
+
   # Garbage collect old generations
   nix.gc = {
     automatic = true;
